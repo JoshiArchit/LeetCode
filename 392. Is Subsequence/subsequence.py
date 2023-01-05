75dbd6fda1e1782372e5b760567d68c06af5795a
architJoshi2904 <aj6082@rit.edu>
2023-01-05 11:28:34 -0500

    LeetCode Problem 392 - Is Subsequence v1.1

--- 392. Is Subsequence/subsequence.py
+++ 392. Is Subsequence/subsequence.py
@@ -7,20 +7,25 @@ Revisions :
 v1.0 - First attempt. Iterative approach.
 Time Complexity : O(T), T = length of target string
 Space Complexity : O(1)
+v1.1 - Using all() function -- NEW
 """
 
 
 def isSubsequence(s: str, t: str) -> bool:
-    c1, c2 = 0,0
-    while c1 < len(s) and c2 < len(t):
-        if s[c1] == t[c2]:
-            c1 += 1
-        c2 += 1
+    # c1, c2 = 0,0
+    # while c1 < len(s) and c2 < len(t):
+    #     if s[c1] == t[c2]:
+    #         c1 += 1
+    #     c2 += 1
+    #
+    # return c1 == len(s)
 
-    return c1 == len(s)
+    # lazy loading using iter()
+    t = iter(t)
+    return all(c in t for c in s)
 
 
 
-s = "abc"
+s = "acb"
 t = "ahbgdc"
 print(isSubsequence(s, t))
